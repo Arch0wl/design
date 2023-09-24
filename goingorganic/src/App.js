656e4481c7a8287c66a652c6bd3b35458b6a8146
Ann Starovoitov <annstarovoitov@gmail.com>
2023-09-24 10:33:17 -0400

    init react app

---
+++ goingorganic/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+]);
+
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
---
+++ goingorganic/src/pages/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomeCss from './Home.module.css';
+
+
+export default function Home() {
+  return (
+    <div>Hello World</div>
+
+  );
+}
